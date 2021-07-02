d4b4e65b3699b4c1ade5d936267bfbbf29042da0
Luke Pflibsen-Jones <luke.timothy.jones@gmail.com>
2021-07-02 16:36:42 -0700

    Add XSS filtering

--- projects/markdown/js/main.js
+++ projects/markdown/js/main.js
@@ -154,10 +154,8 @@ document.addEventListener("DOMContentLoaded", function() {
             html    = ev.data[1],
             md_div  = document.querySelector('div[data-bind="' + bind_id + '"] .display');
         
-        md_div.innerHTML = html;
-        //md_div.innerHTML = purify_html(html);
+        md_div.innerHTML = filterXSS(html);
         PR.prettyPrint();
-        //md_div.innerHTML = purify_html(md_div.innerHTML);
     };
     
     // ----
