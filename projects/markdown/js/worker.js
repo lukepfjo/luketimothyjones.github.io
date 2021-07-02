b49b11e0cfca679531e56af8dda3994981bb040b
Luke Pflibsen-Jones <luke.timothy.jones@gmail.com>
2021-07-02 16:15:01 -0700

    Enable tables, html escapes, and emoji

--- projects/markdown/js/worker.js
+++ projects/markdown/js/worker.js
@@ -1,7 +1,11 @@
 importScripts('showdown.js');
+
 var md_converter = new showdown.Converter();
 
 md_converter.setFlavor('github');
+md_converter.setOption('tables', true);
+md_converter.setOption('backslashEscapesHTMLTags', true);
+md_converter.setOption('emoji', true);
 
 function prettify(source) {
     return source.replace(/(<pre[^>]*>)?[\n\s]?<code([^>]*)>/gi,
