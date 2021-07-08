e043ff99c209f1fe30e3349b95609401c1d92a2f
Luke Pflibsen-Jones <luke.timothy.jones@gmail.com>
2021-07-07 18:41:58 -0700

    Fixed scroll animation!

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* Block until Roboto is loaded in */
 function load_roboto() {
     const font = new FontFace('Roboto Condensed', 'url(https://fonts.gstatic.com/s/robotocondensed/v19/ieVl2ZhZI2eCN5jzbjEETS9weq8-19K7DQ.woff2)');
@@ -5,14 +7,33 @@ function load_roboto() {
     document.fonts.add(font);
 } load_roboto();
 
+
 document.addEventListener('DOMContentLoaded', function() {
-   document.querySelector('#footer-year').innerText = new Date().getFullYear();
+    document.querySelector('#footer-year').innerText = new Date().getFullYear();
    
-   let get_in_touch_btn = document.querySelector('#get-in-touch button');
-   get_in_touch_btn.addEventListener('click', () => {
-       window.open('mailto:luke.pflibsen.jones@gmail.com', '_top');
-   });
-   get_in_touch_btn.addEventListener('mouseenter', () => {
-       get_in_touch_btn.classList.toggle('email-visible');
-   });
+    // Handle email link / display
+    let get_in_touch_btn = document.querySelector('#get-in-touch button');
+    get_in_touch_btn.addEventListener('click', () => {
+        window.open('mailto:luke.pflibsen.jones@gmail.com', '_top');
+    });
+    get_in_touch_btn.addEventListener('mouseenter', () => {
+        get_in_touch_btn.classList.toggle('email-visible');
+    });
+
+    // Handle animation of #position-roller on homepage
+    (function() {
+        const scroll_delay = 1600,
+              scroll_positions = [0, 20, 60, 100, 140];
+
+        let scroll_box = document.querySelector('#position-roller'),
+            target_pos = 1;
+
+        function do_scroll() {
+            scroll_box.scroll({top: scroll_positions[target_pos], behavior: 'smooth'});
+            target_pos = target_pos > 3 ? 0 : target_pos + 1;
+            setTimeout(do_scroll, scroll_delay);
+        }
+
+        setTimeout(do_scroll, scroll_delay);
+    }());
 });
