11314b83dd133942ca045c5e4d3851c557e30a73
Luke Pflibsen-Jones <luke.timothy.jones@gmail.com>
2021-07-30 12:38:30 -0700

    Make MD editor split-screen, code cleanup, overall editor improvements

--- apps/markdown/static/js/worker.js
+++ apps/markdown/static/js/worker.js
@@ -2,18 +2,17 @@ importScripts('showdown.js');
 importScripts('filter-xss.js');
 
 var md_converter = new showdown.Converter();
-
 md_converter.setFlavor('github');
 md_converter.setOption('tables', true);
 md_converter.setOption('backslashEscapesHTMLTags', true);
 md_converter.setOption('emoji', true);
 
+const prettify_regex = /(<pre[^>]*>)?[\n\s]?<code([^>]*)>/gi;
 function prettify(source) {
-    return source.replace(/(<pre[^>]*>)?[\n\s]?<code([^>]*)>/gi,
-        function (match, pre, codeClass) {
+    return source.replace(prettify_regex,
+        function (_, pre, codeClass) {
             if (pre) {
                 return '<pre class="prettyprint linenums"><code' + codeClass + '>';
-        
             } else {
                 return ' <code class="prettyprint">';
             }
@@ -21,10 +20,11 @@ function prettify(source) {
     );
 };
 
+const event_listener_regex = /on\w+=".*"/;
 onmessage = function(ev) {
     let bind_id = ev.data[0],
         mdown   = ev.data[1];
-    
+
     let html = md_converter.makeHtml(mdown);
     html = filterXSS(html, {
         onIgnoreTag: (tag, html, options) => {
@@ -32,12 +32,17 @@ onmessage = function(ev) {
             // If a string is returned, the tag would be replaced with the string
             // If return nothing, the default measure would be taken (specifies using
             // escape, as described below)
-          
+
+            // Markdown uses checkboxes in its formatting
             if (tag === 'input' && html.indexOf('type="checkbox"') !== -1) {
-                // Check if nefarious shit is happening here
+                // Only permit if it does not have an event listener attached
+                if (!event_listener_regex.test(html.toLowerCase())) {
+                    return html;
+                }
             }
         }
     });
+
     postMessage([bind_id, prettify(html)]);
     return;
 };
